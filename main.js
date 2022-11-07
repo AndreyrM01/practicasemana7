d56f831b596b67c7b35601ff9640369718a399e0
Jostin Andrey Ramirez Mayorga <106450347+AndreyrM01@users.noreply.github.com>
2022-11-06 21:11:54 -0600

    Practica de la semana numero 8 javascript

--- main.js
+++ main.js
@@ -1,54 +1,24 @@
-//1
-let j = prompt("Ingrese un numero: ");
-    if (j % 2 === 0) {
-        console.log("par");
-    }  else if(j % 2 === 1){
-        console.log("impar")
-    }
+//Realizar un objeto que tengo 5 propiedades y tres metodos que modifiquen propiedades internas
+const equipodeFut = {
+  portero : 'TerStegen',
+  numerodeEquipacion : 1,
+  defensa : 'Araujo',
+  botines : 'Adidas',
+  medio : 'Pedri',
+  cambios: function(){
+    return 'titular ${this.portero}'
+  },
+  otroDefensa: function(defensa){
+    this.defensa = defensa;
+  },
+  borrar: function(botines){
+    delete this[campo]
+  }
+};
 
-    if(j % 3 ===0) {
-        console.log("divisible entre tres");
-    } else if ( j % 5 === 0 ) {
-        console.log("divisible entre cinco");
-    }
-    console.log(j);
-
-    // ejercicio 2
-     let entrada = prompt('Ingrese un numero: ');
-      let suma = 0;
-        for (let i = 1; i <= entrada; i++) {
-          suma = suma + i;
-      }
-console.log(suma)
-
-    //3
-    while(true){
-      function reverseString(str) {
-          let nuevoString = "";
-          for (let i = str.length - 1; i >= 0; i--) {
-              newString += str[i];
-          }
-          return nuevoString;
-      }
-        break;
-      }
-      const string = prompt('Ingrese un string: ');
-      const result = reverseString(string);
-      console.log(result);
-
-
-      // ejercicio 4
-      let quejodidoejercicio = {
-        a: 'i', e: 'i', i:'i', o: 'i', u: 'i',
-        A: 'I', E: 'I', I:'I', O: 'I', U: 'I'
-      }
-      
-      let cambiaForma = (string) => {
-        return string.replace(/[aeiou]/gi, match => quejodidoejercicio[match])
-      }
-      console.log(cambiaForma('tic tac'));
-    //ejercio 5
-      function orden_alfabeto(str){
-       return str.slit('').sort().join('');
-    }
-    console.log(orden_alfabeto("perro"));
+console.log(cambios);
+console.log(auto.defensa);
+auto.otroDefensa('Kounde');
+console.log(auto.defensa);
+equipodeFut.borrar('botines');
+console.log(equipodeFut);
