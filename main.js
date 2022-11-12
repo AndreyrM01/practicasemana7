f62d59a0bff5ff26e53f7703d30418d34c98f045
Jostin Andrey Ramirez Mayorga <106450347+AndreyrM01@users.noreply.github.com>
2022-11-11 23:03:28 -0600

    tarea-corta

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
-//Realizar un objeto que tengo 5 propiedades y tres metodos que modifiquen propiedades internas
-const equipodeFut = {
-  portero : 'TerStegen',
-  numerodeEquipacion : 1,
-  defensa : 'Araujo',
-  botines : 'Adidas',
-  medio : 'Pedri',
-  cambios: function(){
-    return 'titular ${this.portero}'
-  },
-  otroDefensa: function(defensa){
-    this.defensa = defensa;
-  },
-  borrar: function(botines){
-    delete this[campo]
+let numeroArray = [];
+for (i = 0; i < 5; i++) {
+  let number = parseFloat(prompt("ingrese un numero: "));
+  if(numeroArray === ""){
+      alert('El valor no esta permitido')
+      break;
+  } else {
+    numeroArray.push(parseInt(number));
   }
+}
+
+function minMax (numeroArray){
+  let low = 0;
+  let high = 0;
+  low = numeroArray[0];
+  for (let j = 0; i < numeroArray.length; j++){
+    if (low > numeroArray[j]){
+      low = numeroArray[j];
+    };
+  
+    if (high < valores[j]){
+      high = valores[j];
+    };
+  };
+
+  console.log("El de menor valor es: " + low);
+  console.log("El de mayor valor es: " + high);
 };
 
-console.log(cambios);
-console.log(auto.defensa);
-auto.otroDefensa('Kounde');
-console.log(auto.defensa);
-equipodeFut.borrar('botines');
-console.log(equipodeFut);
+function promedio() {
+  let sum = 0;
+  for (i = 0; i < numeroArray.length; i++) {
+    sum += numeroArray[i];
+  }
+
+  return sum / numeroArray.length;
+}
+
+function med() {
+  if (numeroArray.length % 2 === 0) {
+    let med1 = Math.floor(numeroArray.length / 2);
+
+    return ((numeroArray[med1 - 1] + numeroArray[med1]) / 2).toFixed(2);
+  } else {
+    let med2 = Math.floor((numeroArray.length + 1) / 2);
+
+    return numeroArray[med2 - 1].toFixed(2);
+  }
+}
+console.log("promedio is: " + promedio());
+console.log("mediana is: " + med());
